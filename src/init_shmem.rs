9e0efeec8e3a6a47d3e630999ff8ce9a79be0db3
b0wl <brennanmarxrennie@gmail.com>
2025-08-04 13:10:27 -0400

    init_shmem

--- src/init_shmem.rs
+++ src/init_shmem.rs
@@ -12,14 +12,26 @@ impl SharedMem {
     /// 
     /// Set arg `allow_open_on_error` true if you want to try creating the shmem, and opening it on failure
     pub fn new(name: &str, size: u32, allow_open_on_error: bool) -> Result<Self, Box<dyn std::error::Error>> {
-        let shmem = ShmemConf::new()
-            .size(size.try_into().unwrap())
-            .os_id(name)
-            .create()
-            .or_else(|_| ShmemConf::new().os_id(name).open())?;
-
-        Ok(Self { shmem })
+        if allow_open_on_error {
+            // Try create, if fails try open
+            let shmem = ShmemConf::new()
+                .size(size.try_into().unwrap())
+                .os_id(name)
+                .create()
+                .or_else(|_| ShmemConf::new().os_id(name).open())?;
+    
+            Ok(Self { shmem })
+        } else {
+            // Only try to create
+            let shmem = ShmemConf::new()
+                .size(size.try_into().unwrap())
+                .os_id(name)
+                .create()?; // return error if creation fails
+    
+            Ok(Self { shmem })
+        }
     }
+
 
     /// Get a mutable slice to the shared memory
     pub fn shmem_mut(&mut self) -> &mut [u8] {
